2c41be0f19526b3250d51dda7fb9742906a28239
User <user@gmail.com>
2017-06-06 11:40:38 +0200

    Initial

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Bitbee-purse message queue name
+#define BITCOINURI_QUEUE_NAME "BitbeeURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
